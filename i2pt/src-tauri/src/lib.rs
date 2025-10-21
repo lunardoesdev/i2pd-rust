c05723b2dfa3aa1c2831a6a8182f04a7115e424d
Lunar <lunarlifeburst@gmail.com>
2025-10-21 23:13:47 +1000

    now we are calling i2p!

--- i2pt/src-tauri/src/lib.rs
+++ i2pt/src-tauri/src/lib.rs
@@ -7,21 +7,38 @@ fn greet(name: &str) -> String {
 use i2pd;
 
 #[tauri::command]
-fn starti2pd(args: &str) {
-    i2pd::init(args);
+fn i2pd_start() {
     i2pd::start();
 }
 
 #[tauri::command]
-fn stopi2pd() {
+fn i2pd_stop() {
     i2pd::stop();
 }
+
+#[tauri::command]
+fn i2pd_terminate() {
+    i2pd::terminate();
+}
+
+#[tauri::command]
+fn i2pd_init(args: &str) {
+    i2pd::init(args);
+}
+
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, starti2pd])
+        .invoke_handler(tauri::generate_handler![
+            greet, 
+            i2pd_init,
+            i2pd_start,
+            i2pd_stop,
+            i2pd_terminate
+        ])
         // .invoke_handler(tauri::generate_handler![starti2pd])
         // .invoke_handler(tauri::generate_handler![stopi2pd])
         .run(tauri::generate_context!())
--- i2pt/src/main.ts
+++ i2pt/src/main.ts
@@ -16,11 +16,45 @@ async function greet() {
   }
 }
 
+
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
+  });
+});
+
+
+/// i2pd part
+async function i2pd_init(args: string) {
+  return await invoke("i2pd_init", {
+    args: args
+  })
+}
+
+async function i2pd_start() {
+  return await invoke("i2pd_start")
+}
+
+async function i2pd_stop() {
+  return await invoke("i2pd_stop")
+}
+
+async function i2pd_terminate() {
+  return await invoke("i2pd_terminate")
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  let inp = document.querySelector("#start-i2p-input");
+
+  document.querySelector("#start-i2p-form")?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    let args = (inp! as HTMLInputElement).value
+    await i2pd_stop()
+    await i2pd_terminate()
+    await i2pd_init(args)
+    await i2pd_start()
   });
 });
