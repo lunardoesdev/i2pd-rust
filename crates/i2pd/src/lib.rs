90f99546a4de4b0ed81e2bc3912b48068f335e45
Lunar <lunarlifeburst@gmail.com>
2025-10-21 13:34:24 +1000

    better init api

--- crates/i2pd/src/lib.rs
+++ crates/i2pd/src/lib.rs
@@ -1,7 +1,8 @@
 use i2pd_sys;
 use std::os::raw;
+use std::ffi::CString;
 
-pub fn init(
+pub fn init_raw(
     argc: raw::c_int,
     argv: *mut *mut raw::c_char,
     app_name: *const raw::c_char,
@@ -9,6 +10,21 @@ pub fn init(
     unsafe {
         i2pd_sys::C_InitI2P(argc, argv, app_name);
     }
+}
+
+pub fn init(
+    argc: i32,
+    argv: Vec<&str>,
+    app_name: &str,
+) {
+    let mut cargv: Vec<*mut raw::c_char> = argv.iter()
+        .map(|s| CString::new(*s)
+            .expect("failed to create CString")
+            .into_raw())
+        .collect();
+    
+    let cappname = CString::new(app_name).expect("failed to create CString for appname");
+    init_raw(argc, cargv.as_mut_ptr(), cappname.as_ptr());
 }
 
 pub fn terminate() {
@@ -36,6 +52,7 @@ mod tests {
 
     #[test]
     fn it_works() {
+        init(2, vec!["i2pd", "--help"], "appname");
         start();
     }
 }
