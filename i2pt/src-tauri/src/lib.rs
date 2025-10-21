14703c1a5a639cb7d775c848757eb0fae982c017
Lunar <lunarlifeburst@gmail.com>
2025-10-21 14:37:37 +1000

    add i2pd commands to tauri frontend

--- i2pt/src-tauri/src/lib.rs
+++ i2pt/src-tauri/src/lib.rs
@@ -4,11 +4,26 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use i2pd;
+
+#[tauri::command]
+fn starti2pd(args: &str) {
+    i2pd::init(args);
+    i2pd::start();
+}
+
+#[tauri::command]
+fn stopi2pd() {
+    i2pd::stop();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![starti2pd])
+        .invoke_handler(tauri::generate_handler![stopi2pd])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
